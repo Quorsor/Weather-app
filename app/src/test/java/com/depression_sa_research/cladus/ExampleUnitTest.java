d858a8035201dbd3033f65ecbf61b77f4fc894f7
Quorsor <96727527+Quorsor@users.noreply.github.com>
2022-06-06 22:04:05 +0800

    Upload to GitHub

---
+++ app/src/test/java/com/depression_sa_research/cladus/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.depression_sa_research.cladus;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
